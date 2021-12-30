c3a3dcb4ca0d985dd9e226478fc8abf783de2601
rlduddev <rldudcjswp@naver.com>
2021-12-30 14:55:42 +0900

    vanilla redux

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+}
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleMinus = () => {
+  countStore.dispatch({type: MINUS});
+}
+
+add.addEventListener("click", () => {
+  countStore.dispatch({type: ADD});
+});
+
+minus.addEventListener("click", handleMinus);
+
+
