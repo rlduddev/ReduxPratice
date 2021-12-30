64d713363269bfaa9a37f3eff4096e50c8c94d55
rlduddev <rldudcjswp@naver.com>
2021-12-30 16:19:23 +0900

    vanilla redux toDo list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-
+/* plus, minus */
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
@@ -37,5 +37,84 @@ add.addEventListener("click", () => {
 });
 
 minus.addEventListener("click", handleMinus);
+/* --plus, minus */
 
+/* add todo */
 
+const ul = document.getElementById("list");
+const form = document.getElementById("form");
+const button = document.getElementById("addButton");
+const input = document.getElementById("text");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO"
+
+const addTodo = (text) => {
+  return {
+    type: ADD_TODO,
+    text: text, id: Date.now()
+  }
+}
+
+const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO, 
+    id: id 
+  }
+}
+
+const toDoReducer = (state = [], action) => {
+  console.log(action);
+  switch(action.type) {
+    case ADD_TODO:
+      const newToDoObj = {text: action.text, id: action.id}
+      return [newToDoObj, ...state];
+    case DELETE_TODO:
+      return state.filter(todo => {
+        return todo.id !== parseInt(action.id);
+      });
+    default:
+      return state;
+  }
+}
+
+const toDoStore = createStore(toDoReducer);
+
+toDoStore.subscribe(() => console.log(toDoStore.getState()));
+
+const paintTodo = () => {
+  const todos = toDoStore.getState();
+
+  ul.innerHTML = "";
+
+  todos.forEach(todo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.addEventListener("click", disPatchDeleteTodo);
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+toDoStore.subscribe(paintTodo);
+
+const disPatchAddTodo = (text) => {
+  toDoStore.dispatch(addTodo(text));
+}
+
+const disPatchDeleteTodo = (e) => {
+  const id = e.target.parentNode.id;
+  toDoStore.dispatch(deleteTodo(id));
+}
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  disPatchAddTodo(toDo);
+}
+
+form.addEventListener("submit", onSubmit);
+/* -- add toto */
